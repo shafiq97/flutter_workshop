01ffa173c4ac850cb0b82a34752fc7d14f31b044
Muhammad Shafiq Ahmad Razman <shafiq@Muhammads-MacBook-Air.local>
2023-10-28 11:26:46 +0800

    navigation

---
+++ lib/details_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Details')),
+      body: Center(
+        child: Text('This is the details screen!'),
+      ),
+    );
+  }
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/details');
+          },
+          child: Text('Go to Details'),
+        ),
+      ),
+    );
+  }
+}
