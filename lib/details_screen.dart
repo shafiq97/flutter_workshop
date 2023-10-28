f89f2cb5bcec864191eb605a02eeb28b475911d0
Muhammad Shafiq Ahmad Razman <shafiq@Muhammads-MacBook-Air.local>
2023-10-28 12:11:32 +0800

    lottie example

--- lib/details_screen.dart
+++ lib/details_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class DetailsScreen extends StatelessWidget {
   @override
@@ -8,13 +7,7 @@ class DetailsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Details')),
       body: Center(
-        child: Image.asset(
-          'assets/images/bird.jpg',
-          width: 200.0, // Set the width
-          height: 200.0, // Set the height
-          fit: BoxFit
-              .cover, // This is to ensure the image covers the given dimension without distortion
-        ),
+        child: Lottie.asset('assets/lottie/loading.json'),
       ),
     );
   }
