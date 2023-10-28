aab248eed9a6bc20ef591334c145dcaf479d702b
Muhammad Shafiq Ahmad Razman <shafiq@Muhammads-MacBook-Air.local>
2023-10-28 11:38:27 +0800

    routing

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,15 +9,25 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(title: Text('Home')),
       body: Center(
         child: ElevatedButton(
-          // onPressed: () {
-          //   Navigator.pushNamed(context, '/details');
-          // },
           onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => DetailsScreen()),
-            );
+            Navigator.pushNamed(context, '/details');
           },
+          // onPressed: () {
+          //   Navigator.push(
+          //     context,
+          //     MaterialPageRoute(builder: (context) => DetailsScreen()),
+          //   );
+          // },
+          // onPressed: () {
+          //   // Navigator.pushAndRemoveUntil(context, newRoute, (route) => false);
+          //   Navigator.pushAndRemoveUntil(
+          //     context,
+          //     MaterialPageRoute(
+          //         builder: (BuildContext context) => DetailsScreen()),
+          //     (Route<dynamic> route) =>
+          //         false, // This will remove all previous routes
+          //   );
+          // },
           child: Text('Go to Details'),
         ),
       ),
