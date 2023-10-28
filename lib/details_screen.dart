fe2e2c8ae933a41f490ca6ff786369c09b7dd49a
Muhammad Shafiq Ahmad Razman <shafiq@Muhammads-MacBook-Air.local>
2023-10-28 11:57:26 +0800

    add image szie

--- lib/details_screen.dart
+++ lib/details_screen.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+
 class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Details')),
       body: Center(
-        child: Image.asset('assets/images/bird.jpg'),
+        child: Image.asset(
+          'assets/images/bird.jpg',
+          width: 200.0, // Set the width
+          height: 200.0, // Set the height
+          fit: BoxFit
+              .cover, // This is to ensure the image covers the given dimension without distortion
+        ),
       ),
     );
   }
